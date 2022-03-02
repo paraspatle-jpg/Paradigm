404e202a1d05f97e3e82d042f5ae3116368b188a
paraspatle-jpg <paras18224827@gmail.com>
2022-03-02 21:43:24 +0530

    updated auth controllers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,34 +5,26 @@ require("dotenv").config();
 
 module.exports.login = (req, res) => {
   const { email, password } = req.body;
-  try{
+  try {
     const user = User.getCredentials({ email, password });
     const token = user.generateToken();
 
-    res.status(200).send({user,token})
-  } catch(err){
-
+    res.status(200).send({ user, token });
+  } catch (err) {
+    res.status(400).send({ msg: err });
   }
 };
 
 module.exports.signup = async (req, res) => {
   const { username, email, password } = req.body;
-  User.findOne({ email }).then(async (user) => {
-    if (user)
-      return res
-        .status(400)
-        .json({ message: "User already exists with this mail" });
-
-    newUser.save().then(async (user) => {
-      token = await jwt.sign({ _id: user._id }, "itsasecretbutilovecats");
-      console.log(token);
-      res.status(200).send({
-        userId: user._id,
-        username: user.username,
-        email: user.email,
-      });
-    });
-  });
+  try {
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    const token = await newUser.generateToken();
+    res.status(200).send({ user, token });
+  } catch (err) {
+    res.status(400).send({ msg: err });
+  }
 };
 
 module.exports.getAllUsers = async (req, res) => {
